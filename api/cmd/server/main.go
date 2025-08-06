1a068b4dcc6945435103565123269005740c5be2
Alitsayyed <alitsayyed@gmail.com>
2025-08-06 18:29:59 -0500

    created basic go server

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/AliTSayyed/VULX-AI-Website-Builder/api/internal/application"
+)
+
+func main() {
+	app := application.New()
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Errorf("failed to start app: %w", err)
+	}
+}
